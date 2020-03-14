4c68f84b312a7ed3671ce2d4f3d49d92be4a3228
daku10 <daku.two@gmail.com>
2020-03-14 23:37:31 +0900

    done Adding an Image

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#888", fontSize: 18 }}>
+      <Image
+        source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+        style={styles.logo}
+      />
+      <Text style={styles.insturctions}>
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
@@ -18,5 +22,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10
+  },
+  insturctions: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15
   }
 });
